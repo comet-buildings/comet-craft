f2d9b8e84a8d2a3241e0446c670f65fd19dadab7
Marjorie <aubertmarjorie2@gmail.com>
2023-04-11 14:05:21 +0200

    add chalk with jordane

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+const chalk = require('chalk');
+
 console.log('_________                        __    .__                              _________                _____  __  ._.');
 console.log('\\_   ___ \\  ____   _____   _____/  |_  |  |   _______  __ ____   ______ \\_   ___ \\____________ _/ ____\\/  |_| |');
 console.log('/    \\  \\/ /  _ \\ /     \\_/ __ \\   __\\ |  |  /  _ \\  \\/ // __ \\ /  ___/ /    \\  \\/\\_  __ \\__  \\\\   __\\\\   __\\ |');
@@ -7,14 +9,14 @@ console.log(' \\______  /\\____/|__|_|  /\\___  >__|   |____/\\____/ \\_/  \\___
 console.log('        \\/             \\/     \\/                             \\/     \\/          \\/            \\/             \\/');
 
 
-console.log("Jouez et gagnez une journée chez Comet !")
+console.log(chalk.blue("Jouez et gagnez une journée chez Comet !"))
 console.log("https://docs.google.com/forms/d/e/1FAIpQLSfmpRF9xTDbJHfRKBQJU2YFy5gsVavuDCa9gXF6vkoSV4DSZQ/viewform")
 
 console.log("L'équipe Comet à Devoxx")
 
 console.log('Mercredi NodeJS : patterns et outils pour partir en production sereinement - Alexandre VICTOOR https://cfp.devoxx.fr/2023/talk/NBS-8618/NodeJS_:_patterns_et_outils_pour_partir_en_production_sereinement')
 console.log('Mercredi Avec Zod, luttez contre l’any-gration à vos frontières - Jordane GRENAT https://cfp.devoxx.fr/2023/talk/OCF-4410/Avec_Zod,_luttez_contre_l%E2%80%99any-gration_a_vos_frontieres')
-console.log('Vendredi Remix Workshop -  Johann RAKOTOHARISOA et Johan ROUVE https://cfp.devoxx.fr/2023/talk/JAI-5123/Remix_Workshop')
+console.log('Vendredi Remix Workshop - Johann RAKOTOHARISOA et Johan ROUVE https://cfp.devoxx.fr/2023/talk/JAI-5123/Remix_Workshop')
 console.log("Vendredi Mob et Extreme programming : REX d'une développeuse junior - Marjorie AUBERT https://cfp.devoxx.fr/2023/talk/TUD-2583/Mob_et_Extreme_programming_:_REX_d%E2%80%99une_developpeuse_junior ")
 
 
