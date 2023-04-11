94411f6aa1f77fe59239f7412963e059f0765085
Marjorie <aubertmarjorie2@gmail.com>
2023-04-11 13:57:19 +0200

    add devoxx

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log(' \\______  /\\____/|__|_|  /\\___  >__|   |____/\\____/ \\_/  \\___
 console.log('        \\/             \\/     \\/                             \\/     \\/          \\/            \\/             \\/');
 
 
-console.log("Jouez et gagnez une journée chez Comet ! ")
+console.log("Jouez et gagnez une journée chez Comet !")
 console.log("https://docs.google.com/forms/d/e/1FAIpQLSfmpRF9xTDbJHfRKBQJU2YFy5gsVavuDCa9gXF6vkoSV4DSZQ/viewform")
 
 console.log("L'équipe Comet à Devoxx")
@@ -16,3 +16,10 @@ console.log('Mercredi NodeJS : patterns et outils pour partir en production sere
 console.log('Mercredi Avec Zod, luttez contre l’any-gration à vos frontières - Jordane GRENAT https://cfp.devoxx.fr/2023/talk/OCF-4410/Avec_Zod,_luttez_contre_l%E2%80%99any-gration_a_vos_frontieres')
 console.log('Vendredi Remix Workshop -  Johann RAKOTOHARISOA et Johan ROUVE https://cfp.devoxx.fr/2023/talk/JAI-5123/Remix_Workshop')
 console.log("Vendredi Mob et Extreme programming : REX d'une développeuse junior - Marjorie AUBERT https://cfp.devoxx.fr/2023/talk/TUD-2583/Mob_et_Extreme_programming_:_REX_d%E2%80%99une_developpeuse_junior ")
+
+
+
+console.log('   /   \_____   _______  ____  __');
+console.log('  / /\ / _ \ \ / / _ \ \/ /\ \/ /');
+console.log(' / /_//  __/\ V / (_) >  <  >  < ');
+console.log("/___,' \___| \_/ \___/_/\_\/_/\_\.")
